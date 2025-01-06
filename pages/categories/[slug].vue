a5bc4f8b57956ac6b3162ae86abfb297dcaee575
biletweb-macmini <bilet.igor@gmail.com>
2025-01-06 20:56:33 +0200

    fix

--- pages/categories/[slug].vue
+++ pages/categories/[slug].vue
@@ -25,7 +25,7 @@ const limit = 10
 const categorySlug = useRoute().params.slug
 
 const { data, error, status } = await useFetch(`http://127.0.0.1:8000/api/categories/${categorySlug}/subcategories`, {
-// const { data, error, status } = await useLazyFetch(`http://127.0.0.1:8000/api/categories/${categorySlug}/subcategories`, {
+  // const { data, error, status } = await useLazyFetch(`http://127.0.0.1:8000/api/categories/${categorySlug}/subcategories`, {
   query: {
     offset: offset.value,
     limit: limit
