6e206af798f2b5295cd9bc5d160cfa2148014e63
biletweb-macmini <bilet.igor@gmail.com>
2025-01-06 21:05:53 +0200

    fix

--- pages/categories/[slug].vue
+++ pages/categories/[slug].vue
@@ -1,9 +1,9 @@
 <template>
-  <div v-if="error">Произошла ошибка при загрузке данных: {{ error.message }}</div>
+  <div v-if="status === 'pending'" class="text-center">Loading...</div>
+  <div v-else-if="error">Произошла ошибка при загрузке данных: {{ error.message }}</div>
   <div v-else>
-    <div class="mb-4 text-3xl font-bold">{{ data.categoryName }} ID: {{ $route.params.slug }}</div>
-    <div v-if="status === 'pending'" class="text-center">Loading...</div>
-    <div v-else class="grid grid-cols-5 gap-4">
+    <div class="mb-4 text-3xl font-bold">{{ data.categoryName }}</div>
+    <div class="grid grid-cols-5 gap-4">
       <div v-for="category in data.categories" :key="category.id">
         <NuxtLink :to="`/categories/${category.id}`">
           <div class="card rounded-lg bg-slate-300 p-4 text-center">
@@ -24,8 +24,8 @@ const offset = ref(0)
 const limit = 10
 const categorySlug = useRoute().params.slug
 
-const { data, error, status } = await useFetch(`http://127.0.0.1:8000/api/categories/${categorySlug}/subcategories`, {
-  // const { data, error, status } = await useLazyFetch(`http://127.0.0.1:8000/api/categories/${categorySlug}/subcategories`, {
+// const { data, error, status } = await useFetch(`http://127.0.0.1:8000/api/categories/${categorySlug}/subcategories`, {
+const { data, error, status } = await useLazyFetch(`http://127.0.0.1:8000/api/categories/${categorySlug}/subcategories`, {
   query: {
     offset: offset.value,
     limit: limit
--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="mb-4 text-3xl font-bold">Categories</div>
   <div v-if="status === 'pending'" class="text-center">Loading...</div>
-  <div v-else class="grid grid-cols-5 gap-4">
-    <div v-for="category in data.categories" :key="category.id">
-      <NuxtLink :to="`/categories/${category.id}`">
-        <div class="card rounded-lg bg-slate-300 p-4 text-center">
-          {{ category.name }}
-        </div>
-      </NuxtLink>
+  <div v-else-if="error">Произошла ошибка при загрузке данных: {{ error.message }}</div>
+  <div v-else>
+    <div class="mb-4 text-3xl font-bold">Categories</div>
+    <div class="grid grid-cols-5 gap-4">
+      <div v-for="category in data.categories" :key="category.id">
+        <NuxtLink :to="`/categories/${category.id}`">
+          <div class="card rounded-lg bg-slate-300 p-4 text-center">
+            {{ category.name }}
+          </div>
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
@@ -17,6 +20,6 @@ useHead({
   title: 'Categories'
 })
 
-const { data, status } = await useFetch('http://127.0.0.1:8000/api/categories')
-// const { data, status } = await useLazyFetch('http://127.0.0.1:8000/api/categories')
+// const { data, status } = await useFetch('http://127.0.0.1:8000/api/categories')
+const { data, error, status } = await useLazyFetch('http://127.0.0.1:8000/api/categories')
 </script>
