a79777aa4ec98665dfe2b4e0584469428b6d8890
biletweb-macmini <bilet.igor@gmail.com>
2025-01-08 15:50:36 +0200

    fix

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="rounded-lg bg-slate-300 p-4">
-    {{ product.name }}
-    <hr class="my-2" />
-    <span>Цена: {{ product.price }}</span>
+  <div class="rounded-lg bg-slate-100 p-4">
+    <div class="mb-4 flex items-center justify-between">
+      <span class="rounded-lg bg-rose-500 px-2 py-1 text-xs text-white">SALE</span>
+      <Icon name="mingcute:heart-line" class="text-yellow-500 hover:cursor-pointer hover:text-yellow-600" size="24px" />
+    </div>
+    <div class="mb-4">
+      <img src="/tv.webp" :alt="product.name" class="w-full" />
+    </div>
+    <div class="mb-4">
+      {{ product.name }}
+    </div>
+    <div class="flex items-center justify-between">
+      <span class="font-semibold">{{ product.price }} ₴</span>
+      <Icon name="mingcute:shopping-cart-2-line" class="text-green-500 hover:cursor-pointer hover:text-green-600" size="24px" />
+    </div>
   </div>
 </template>
 
