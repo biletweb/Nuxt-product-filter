0dce77f3bf7ea9b945bae698148e8c7caf191540
biletweb-macmini <bilet.igor@gmail.com>
2025-01-08 16:39:18 +0200

    fix

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -10,8 +10,11 @@
     <div class="mb-4">
       {{ product.name }}
     </div>
-    <div class="flex items-center justify-between">
-      <span class="font-semibold">{{ product.price }} ₴</span>
+    <div class="flex items-end justify-between">
+      <div class="flex flex-col">
+        <span class="text-sm text-slate-400 line-through">{{ product.price }} ₴</span>
+        <span class="-mt-1 font-semibold">{{ product.price }} ₴</span>
+      </div>
       <Icon name="mingcute:shopping-cart-2-line" class="text-green-500 hover:cursor-pointer hover:text-green-600" size="24px" />
     </div>
   </div>
