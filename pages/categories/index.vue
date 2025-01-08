9a0dc252641c7b29e25a50a5b95da8a0e0839833
biletweb-macmini <bilet.igor@gmail.com>
2025-01-08 03:24:31 +0200

    fix

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -6,15 +6,7 @@
   <div v-else>
     <div class="my-4 text-3xl font-bold">Categories</div>
     <div v-if="status === 'pending'" class="text-center">Loading...</div>
-    <div v-else class="grid grid-cols-5 gap-4">
-      <div v-for="category in data.categories" :key="category.id">
-        <NuxtLink :to="`/categories/${category.slug}`">
-          <div class="card rounded-lg bg-slate-300 p-4 text-center">
-            {{ category.name }}
-          </div>
-        </NuxtLink>
-      </div>
-    </div>
+    <CategoryList v-else :categories="data.categories" />
   </div>
 </template>
 
