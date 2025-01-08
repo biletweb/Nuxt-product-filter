9b2573ba149e51a776539a91a3b8c5ae2e9eff32
biletweb-macmini <bilet.igor@gmail.com>
2025-01-08 22:40:16 +0200

    fix

--- components/CategoryFilters.vue
+++ components/CategoryFilters.vue
@@ -3,7 +3,7 @@
     <div v-for="(filter, index) in categoryFilters" :key="filter.id" :class="{ 'mb-2': index !== categoryFilters.length - 1 }">
       <span class="font-bold">{{ filter.name }}</span>
       <div v-for="value in filter.values" :key="value.id" class="flex items-center">
-        <input type="checkbox" :value="value.id" :id="value.id" />
+        <input type="checkbox" :value="value.id" :id="value.id" @change="handleFilterChange(filter.id, value.id)" />
         <label :for="value.id" class="ms-1">{{ value.value }}</label>
       </div>
     </div>
@@ -17,4 +17,22 @@ const props = defineProps({
     required: true
   }
 })
+
+const selectedFilters = reactive({})
+
+const handleFilterChange = (filterId, valueId) => {
+  if (!selectedFilters[filterId]) {
+    selectedFilters[filterId] = []
+  }
+  const filter = selectedFilters[filterId]
+  if (filter.includes(valueId)) {
+    const index = filter.indexOf(valueId)
+    filter.splice(index, 1)
+    if (filter.length === 0) {
+      delete selectedFilters[filterId]
+    }
+  } else {
+    filter.push(valueId)
+  }
+}
 </script>
