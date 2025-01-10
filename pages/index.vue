1d749e86c29b7d2e582e098faa241ee648e208d5
biletweb-macmini <bilet.igor@gmail.com>
2025-01-11 00:18:07 +0200

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,13 @@
   <div>
     <div class="my-4 text-3xl font-bold dark:text-red-500">Hello Nuxt 3</div>
 
-    <div>
+    <!-- <div>
       <select v-model="$colorMode.preference">
         <option value="system">System</option>
         <option value="light">Light</option>
         <option value="dark">Dark</option>
       </select>
-    </div>
+    </div> -->
 
     <div class="relative">
       <div>
@@ -24,7 +24,7 @@
       <div v-if="showUserRoleFilter" class="absolute start-4 top-6 z-10 mt-2 rounded-lg border border-slate-300 bg-slate-100">
         <div class="p-2">
           <button
-            @click=""
+            @click="setColorMode('system')"
             type="button"
             class="flex w-full cursor-pointer items-center rounded-lg px-4 py-2 text-sm hover:bg-slate-50"
           >
@@ -32,7 +32,7 @@
             <span>System</span>
           </button>
           <button
-            @click=""
+            @click="setColorMode('light')"
             type="button"
             class="flex w-full cursor-pointer items-center rounded-lg px-4 py-2 text-sm hover:bg-slate-50"
           >
@@ -40,7 +40,7 @@
             <span>Light</span>
           </button>
           <button
-            @click=""
+            @click="setColorMode('dark')"
             type="button"
             class="flex w-full cursor-pointer items-center rounded-lg px-4 py-2 text-sm hover:bg-slate-50"
           >
@@ -59,5 +59,10 @@ const colorMode = useColorMode()
 
 const toggleUserRoleFilter = () => {
   showUserRoleFilter.value = !showUserRoleFilter.value
+}
+
+const setColorMode = (mode) => {
+  colorMode.preference = mode
+  toggleUserRoleFilter()
 }
 </script>
