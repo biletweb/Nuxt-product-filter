333ad4fa349bd9e431a6910fb4d36e6ca57e453b
biletweb-macmini <bilet.igor@gmail.com>
2025-01-11 20:51:00 +0200

    fix

--- error.vue
+++ error.vue
@@ -3,7 +3,7 @@
     <div class="mx-auto w-6/12 rounded-lg border bg-slate-100 p-4 text-center">
       <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
       <p class="mt-7 text-6xl">Ooops.</p>
-      <p class="mt-7 font-semibold text-rose-500">{{ error.message }}</p>
+      <p class="mt-7 font-semibold text-red-500">{{ error.message }}</p>
       <button class="my-7 rounded-lg bg-sky-500 px-4 py-2 text-white hover:bg-sky-600" @click="handleClearError">Go home</button>
     </div>
   </div>
