b82beed740617d5b26f2aea1ddd9a4dc1c9d41ea
biletweb-macmini <bilet.igor@gmail.com>
2025-01-12 00:25:54 +0200

    fix

--- components/LangSwitcher.vue
+++ components/LangSwitcher.vue
@@ -7,14 +7,18 @@
 <script setup>
 const { locale, locales, setLocale } = useI18n()
 
+// Получаем имя текущей локали из списка доступных
 const currentLocaleName = computed(() => {
   const current = locales.value.find((l) => l.code === locale.value)
   return current ? current.name : locale.value
 })
 
 const toggleLocale = () => {
+  // Находим индекс текущей локали в списке локалей
   const currentIndex = locales.value.findIndex((l) => l.code === locale.value)
+  // Получаем индекс следующей локали, используя цикл по списку
   const nextIndex = (currentIndex + 1) % locales.value.length
+  // Устанавливаем следующую локаль
   setLocale(locales.value[nextIndex].code)
 }
 </script>
