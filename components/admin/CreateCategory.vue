f1a0dd4d0fc53b4115f4bdc169bc132e66ad1fed
biletweb-macmini <bilet.igor@gmail.com>
2025-01-11 19:44:24 +0200

    fix

--- components/admin/CreateCategory.vue
+++ components/admin/CreateCategory.vue
@@ -2,7 +2,7 @@
   <div class="rounded-lg border border-slate-300 bg-slate-100 p-4">
     <div class="flex items-center justify-between">
       <div class="text-xl font-bold">Create category</div>
-      <div class="rounded-lg border border-green-100 bg-green-50 p-1 text-xs text-green-500">
+      <div v-if="successResponse" class="rounded-lg border border-green-100 bg-green-50 p-1 text-xs text-green-500">
         {{ successResponse }}
       </div>
     </div>
