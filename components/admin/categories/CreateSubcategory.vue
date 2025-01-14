0ac77072ca4fc143f81a7fc2d78285f613839b2b
biletweb-macmini <bilet.igor@gmail.com>
2025-01-14 17:58:06 +0200

    fix

--- components/admin/categories/CreateSubcategory.vue
+++ components/admin/categories/CreateSubcategory.vue
@@ -6,7 +6,7 @@
         {{ successResponse }}
       </div>
     </div>
-    <form @submit.prevent="">
+    <form @submit.prevent="createSubcategory">
       <div class="grid grid-cols-2 gap-4">
         <div class="relative">
           <label for="category" class="ms-1 text-sm"> {{ $t('Name') }}<sup class="text-red-500">*</sup> </label>
@@ -100,7 +100,7 @@
             <li class="px-4 py-2">{{ $t('Nothing found') }}</li>
           </ul>
         </div>
-        <div v-if="subcategoryId" class="relative col-span-2">
+        <div v-if="subcategoryId" class="relative">
           <label for="name" class="ms-1 text-sm">{{ $t('Name') }}<sup class="text-red-500">*</sup></label>
           <div class="absolute left-2.5 top-[33px] text-gray-400">
             <Icon name="mingcute:folder-open-line" size="24px" />
@@ -119,10 +119,48 @@
           />
           <p v-if="errorField === 'name'" class="ms-1 mt-1 text-xs text-red-500">{{ errorResponse }}</p>
         </div>
+        <div v-if="subcategoryId" class="relative">
+          <label for="name" class="ms-1 text-sm">{{ $t('Slug') }}</label>
+          <div class="absolute left-2.5 top-[33px] text-gray-400">
+            <Icon name="mingcute:copy-3-line" size="24px" />
+          </div>
+          <input
+            v-model="slug"
+            type="text"
+            name="slug"
+            id="slug"
+            :placeholder="$t('Slug')"
+            class="w-full rounded-lg border p-2 pl-10 focus:outline-none"
+            :class="{
+              'focus:border-sky-500': errorField !== 'slug',
+              'border-red-500': errorField === 'slug'
+            }"
+          />
+          <p v-if="errorField === 'slug'" class="ms-1 mt-1 text-xs text-red-500">{{ errorResponse }}</p>
+        </div>
+        <div v-if="subcategoryId" class="relative col-span-2">
+          <label for="description" class="ms-1 text-sm">{{ $t('Description SEO') }}<sup class="text-red-500">*</sup></label>
+          <div class="absolute left-2.5 top-[33px] text-gray-400">
+            <Icon name="mingcute:information-line" size="24px" />
+          </div>
+          <textarea
+            v-model="description"
+            name="description"
+            id="description"
+            :placeholder="$t('Description SEO')"
+            class="min-h-[42px] w-full rounded-lg border p-2 pl-10 focus:outline-none"
+            :class="{
+              'focus:border-sky-500': errorField !== 'description',
+              'border-red-500': errorField === 'description'
+            }"
+            rows="2"
+          />
+          <p v-if="errorField === 'description'" class="ms-1 mt-1 text-xs text-red-500">{{ errorResponse }}</p>
+        </div>
       </div>
       <div v-if="subcategoryId" class="mt-4 flex justify-end">
-        <button type="submit" class="rounded-lg bg-sky-500 px-4 py-2 text-white hover:bg-sky-600" :disabled="loading">
-          <Icon v-if="loading" name="svg-spinners:8-dots-rotate" size="24px" class="flex" />
+        <button type="submit" class="rounded-lg bg-sky-500 px-4 py-2 text-white hover:bg-sky-600" :disabled="loadingCreateSubcategory">
+          <Icon v-if="loadingCreateSubcategory" name="svg-spinners:8-dots-rotate" size="24px" class="flex" />
           <span v-else>{{ $t('Create') }}</span>
         </button>
       </div>
@@ -134,6 +172,8 @@
 const categoryName = ref('')
 const subcategoryName = ref('')
 const name = ref('')
+const slug = ref('')
+const description = ref('')
 const categoryId = ref('')
 const subcategoryId = ref('')
 const successResponse = ref(null)
@@ -143,6 +183,7 @@ const filteredCategories = ref([])
 const filteredSubcategories = ref([])
 const config = useRuntimeConfig()
 const loadingGetSubcategories = ref(false)
+const loadingCreateSubcategory = ref(false)
 const subcategories = ref([])
 
 const { data, status, error } = await useLazyFetch(config.public.backendUrl + `/admin/category/get-categories`, {
@@ -220,6 +261,42 @@ const getSubcategories = async (categoryId) => {
     })
   } finally {
     loadingGetSubcategories.value = false
+  }
+}
+
+const createSubcategory = async () => {
+  loadingCreateSubcategory.value = true
+  errorField.value = null
+  errorResponse.value = null
+  successResponse.value = null
+  try {
+    const response = await $fetch(config.public.backendUrl + `/admin/category/create-subcategory`, {
+      method: 'POST',
+      body: {
+        name: name.value,
+        slug: slug.value,
+        description: description.value,
+        categoryId: categoryId.value,
+        subcategoryId: subcategoryId.value
+      },
+      timeout: 5000
+    })
+    if (response.error) {
+      errorResponse.value = response.error
+      errorField.value = response.field
+    } else {
+      successResponse.value = response.message
+      name.value = ''
+      slug.value = ''
+      categoryName.value = ''
+    }
+  } catch (error) {
+    showError({
+      statusCode: error.statusCode,
+      statusMessage: error.response?._data?.name || error.statusMessage || 'Request aborted due to timeout'
+    })
+  } finally {
+    loadingCreateSubcategory.value = false
   }
 }
 </script>
