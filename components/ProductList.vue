dcd77b71d7ac12a1800ba63b65ddbfcab425ca02
biletweb-macmini <bilet.igor@gmail.com>
2025-01-17 02:25:37 +0200

    fix

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -1,4 +1,7 @@
 <template>
+  <div v-if="!products.length" class="rounded-lg border border-slate-300 bg-slate-100 p-4 text-center uppercase">
+    {{ $t('No products') }}
+  </div>
   <div class="grid grid-cols-5 gap-4">
     <div v-for="product in products" :key="product.id">
       <ProductCard :product="product" />
